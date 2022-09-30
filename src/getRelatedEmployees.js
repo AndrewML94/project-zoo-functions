0a8138fde8a6f180080b525e9c37ed59911c092a
Andrew <andrewmll94@gmail.com>
2022-09-29 22:56:19 -0300

    Conclusão do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+  const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+  const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+  const managers = [stephanieId, olaId, burlId];
+  return managers.some((manager) => id === manager);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const error = isManager(managerId);
+  if (error !== true) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const result = [];
+  data.employees.forEach((name) => name.managers.filter((people) =>
+    (people === managerId ? result.push(`${name.firstName} ${name.lastName}`) : '')));
+  return result;
 }
 
 module.exports = { isManager, getRelatedEmployees };
