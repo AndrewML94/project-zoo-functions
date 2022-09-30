9d2177d120e3ced25e73b2da3154a7675ddf69b2
Andrew <andrewmll94@gmail.com>
2022-09-30 16:55:45 -0300

    Conclusão dos requisitos 6, 7 e 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,25 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica a quantidade de elefantes retornados.', () => {
+    expect(handlerElephants('count')).toEqual(4);
+    expect(handlerElephants()).toEqual(undefined);
+    expect(handlerElephants('')).toEqual(null);
+    expect(handlerElephants(4)).toEqual('Parâmetro inválido, é necessário uma string');
+  });
+  it('Verifica se é retornado um array com o nome de todos os elefantes.', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Verifica a idade média dos elefantes.', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+  it('Verifica a localização dos elefantes.', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+  it('Verifica a popularidade dos elefantes.', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  });
+  it('Verifica os dias que os elefantes estarão disponíveis.', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+});
