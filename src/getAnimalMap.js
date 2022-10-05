8de1a298e0da378b8fd7602e8e3cef3bbb7c3e02
Andrew <andrewmll94@gmail.com>
2022-10-04 21:15:51 -0300

    Conclusão do requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function animalList() {
+  const result = { NE: [], NW: [], SE: [], SW: [] };
+  data.species.forEach((a) => result[a.location].push(a.name));
+  return result;
 }
+
+function includeNames(param) {
+  const result = { NE: [], NW: [], SE: [], SW: [] };
+  if (param.sorted) {
+    data.species.forEach((a) => result[a.location]
+      .push({ [a.name]: a.residents.map((b) => b.name).sort() }));
+    return result;
+  }
+  data.species.forEach((a) => result[a.location]
+    .push({ [a.name]: a.residents.map((b) => b.name) }));
+  return result;
+}
+
+function sex(param) {
+  const result = { NE: [], NW: [], SE: [], SW: [] };
+  if (param.sorted) {
+    data.species.forEach((a) => result[a.location].push({ [a.name]: a.residents
+      .filter((b) => b.sex === param.sex).map((c) => c.name).sort() }));
+    return result;
+  }
+  data.species.forEach((a) => result[a.location]
+    .push({ [a.name]: a.residents.filter((b) => b.sex === param.sex).map((c) => c.name) }));
+  return result;
+}
+
+function getAnimalMap(options) {
+  if (options === undefined || options.includeNames === undefined) {
+    return animalList();
+  }
+  if (options.includeNames && !options.sex) {
+    return includeNames(options);
+  }
+  return sex(options);
+}
+
+console.log(getAnimalMap({ includeNames: true, sex: 'male', sorted: true }));
 
 module.exports = getAnimalMap;
